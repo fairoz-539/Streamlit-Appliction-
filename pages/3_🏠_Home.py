6f090f4b0fbc1308eea6d70e26900a95d28cb1d8
Fairoz Ahmed <129158093+fairoz-539@users.noreply.github.com>
2024-05-12 21:48:35 +0530

    Update 3_🏠_Home.py

--- pages/3_🏠_Home.py
+++ pages/3_🏠_Home.py
@@ -180,6 +180,21 @@ def generate_pdf(question, answer, name, rno, sec, sub, assign_num, font_family=
 
 
 st.title("Assignment Writer App")
+st.info('Fill this form for Your front page details', icon="ℹ️")
+with st.form("form1", clear_on_submit=True, border=True):
+    name = st.text_input("Enter Your Name: ")
+    rno = st.text_input("Enter Your Roll No: ")
+    sec = st.text_input("Enter Your Section: ")
+    sub = st.text_input("Enter Your subject title: ")
+    assign_num = st.selectbox("Select Assignment number: ", [1, 2])
+    btn = st.form_submit_button("Submit")
+
+if btn:
+    if not name or not rno or not sub or not assign_num:
+        st.error("Please fill out all required fields!")
+    else:
+        with open("form_values.txt", "w") as file:
+            st.success("Form submitted successfully!")
 st.warning("Please fill the front details if not filled.")
 # Get user input
 question = st.text_input("Enter Your Question:")
