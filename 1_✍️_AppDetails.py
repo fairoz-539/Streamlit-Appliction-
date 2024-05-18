8bcd57a706cb29cd716f2d2d8558a0ed161e7763
Fairoz Ahmed <129158093+fairoz-539@users.noreply.github.com>
2024-05-18 23:34:45 +0530

    Update 1_✍️_AppDetails.py

--- 1_✍️_AppDetails.py
+++ 1_✍️_AppDetails.py
@@ -525,12 +525,12 @@ if gen_answer:
 
 if st.session_state.Gen_Ans and st.session_state.Answer:
     answer = st.session_state.Answer
-    col3, col4 = st.columns(2)
+    col3 = st.columns(1)
 
+    # with col3:
+    #     st.title(st.session_state.question)
+    #     st.write(answer)
     with col3:
-        st.title(st.session_state.question)
-        st.write(answer)
-    with col4:
         # Display answer in HTML format
         st.markdown(
             f"<h1 style='font-family:{st.session_state.font_family};'>{st.session_state.question}</h1><p style='font-family:{st.session_state.font_family}'>{answer}</p>",
