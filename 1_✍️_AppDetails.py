189f8250a44b4ae93b388202ee816407abf27e0d
Fairoz Ahmed <129158093+fairoz-539@users.noreply.github.com>
2024-05-23 20:48:05 +0530

    Update 1_✍️_AppDetails.py

--- 1_✍️_AppDetails.py
+++ 1_✍️_AppDetails.py
@@ -29,7 +29,7 @@ hds = """<style>
      opacity: 0.8;
     }
     p,h1{
-    font-family: Arial;
+    font-family: Source sans pro,Monospace;
     }
     div[data-testid="stSidebarContent"] {
         background-color: #052958; /* Text color */
@@ -39,7 +39,7 @@ hds = """<style>
         background-color: #052958; /* Text color */
     }
     span{
-     font-family: Arial;
+     font-family: Source sans pro,monospace;
      }
     input[type="text"], input[type="number"], input[type="email"], input[type="password"], textarea, select, .st-aw{
         background-color: #0c1835;
